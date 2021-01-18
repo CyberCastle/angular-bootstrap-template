bf9dd394f78f1a8bdc54bcef2c39694cead2bf34
CyberCastle <cybercastle@gmail.com>
2021-01-18 17:47:47 -0300

    Se añade un ejemplo elemental...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-bootstrap-template';
 
+  capturedImage!: string;
+
   print(): void {
-    window.print();
+    const printArea = document.querySelector('#print-area') as HTMLElement;
+
+    if (printArea)
+      html2canvas(printArea).then((canvas) => {
+        this.capturedImage = canvas.toDataURL('image/png');
+        console.log('canvas.toDataURL() -->' + this.capturedImage);
+        const doc = new jsPDF();
+        doc.addImage(
+          this.capturedImage,
+          'PNG',
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+        doc.save('test.pdf');
+      });
   }
 }
